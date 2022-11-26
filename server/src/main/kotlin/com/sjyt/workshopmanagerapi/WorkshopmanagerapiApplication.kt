52134418fca7bd69eb52b802307179835700355e
Yuta Shoji <yuta_shoji@mail.toyota.co.jp>
2022-11-26 19:27:43 +0900

    applicationにserverディレクトリを新たに追加した

---
+++ server/src/main/kotlin/com/sjyt/workshopmanagerapi/WorkshopmanagerapiApplication.kt
@@ -0,0 +1,11 @@
+package com.sjyt.workshopmanagerapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WorkshopmanagerapiApplication
+
+fun main(args: Array<String>) {
+	runApplication<WorkshopmanagerapiApplication>(*args)
+}
---
+++ server/src/test/kotlin/com/sjyt/workshopmanagerapi/WorkshopmanagerapiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sjyt.workshopmanagerapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WorkshopmanagerapiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
