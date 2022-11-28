0fd726272258f9a94f02f4c100e66fff9202dba4
Yuta Shoji <yuta_shoji@mail.toyota.co.jp>
2022-11-28 22:54:49 +0900

    動かないbackendを作成した

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -20,6 +20,10 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+	implementation("org.postgresql:postgresql")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 }
 
 tasks.withType<KotlinCompile> {
